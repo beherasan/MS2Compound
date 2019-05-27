f7157c1ca35bacd44bec8155dc5c5f15b30f2006
Santosh Behera <beheras40@gmail.com>
2019-05-27 21:39:38 +0530

    Add files via upload

---
+++ relativeIntensityMGF.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $infile=$ARGV[0];
+open(IN,"$infile") or die "could not open the file:$!\n";
+
+my $max_intensity;
+my @mz_arr;my @int_arr;
+
+while(<IN>)
+{
+	chomp;
+	$_=~s/\r//g;
+	if(/^[^0-9]/ && /^[^(END IONS)]/)
+	{
+		print "$_\n";
+		$max_intensity=0;
+		@mz_arr=@int_arr=();
+	}
+	elsif(/^[0-9]/)
+	{
+		my ($mz,$int)=(split /\s/)[0,1];
+		if($int >= $max_intensity)
+		{
+			$max_intensity = $int;
+		}
+		push(@mz_arr,$mz);
+		push(@int_arr,$int);
+	}
+	elsif(/^END IONS/)
+	{
+		for (my $i=0;$i < scalar(@mz_arr);$i++)
+		{
+			print "$mz_arr[$i]";
+			my $relativeInt=($int_arr[$i]/$max_intensity)*100;
+			print " $relativeInt\n";
+		}
+		print "END IONS\n\n";
+	}
+}
+
+close IN;
